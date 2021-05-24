6db43f1a87e5bb1c8d957c7dd19b69af0cf4fa78
nodiscc <nodiscc@gmail.com>
2021-05-24 14:40:52 +0200

    utils.sh: always output package descriptions in english

--- utils.sh
+++ utils.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function _getdescription() {
     package="$1"
-    apt-cache show "$package" | grep -E "Description(-en|-fr)" | cut -d" " -f1 --complement | head -n1
+    LANG=C apt-cache show "$package" | grep -E "Description(-en|-fr)" | cut -d" " -f1 --complement | head -n1
 }
 
 moinmoin_markup=$(grep -E "#|◼|^$" README.md | sed -e 's/#/=/g' | sed 's/^\(=*\)\(.*\)/\1\2 \1/' |  sed 's|.*https://packages.debian.org/sid/\(.*\))).*|\1|g' | sed 's/^\([Aa-Zz]\)/ \* DebPkg:\1/')
