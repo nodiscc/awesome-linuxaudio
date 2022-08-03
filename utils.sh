f0822ed613ed915c6f013c36a20dc4bb5286e8d6
nodiscc <nodiscc@gmail.com>
2022-08-03 21:17:29 +0200

    tools/utils.sh: indentation/line wrap

--- utils.sh
+++ utils.sh
@@ -4,6 +4,17 @@ function _getdescription() {
     LANG=C apt-cache show "$package" | grep -E "Description(-en|-fr)" | cut -d" " -f1 --complement | head -n1
 }
 
-moinmoin_markup=$(grep -E "#|◼|^$" README.md | sed -e 's/#/=/g' | sed 's/^\(=*\)\(.*\)/\1\2 \1/' |  sed 's|.*https://packages.debian.org/sid/\(.*\))).*|\1|g' | sed 's/^\([Aa-Zz]\)/ \* DebPkg:\1/')
-echo "$moinmoin_markup" | while read line; do if [[ "$line" =~ DebPkg ]]; then package=${line#*:}; echo " * DebPkg:$package - $(_getdescription $package)"; else echo "$line"; fi; done
+moinmoin_markup=$(grep -E "#|◼|^$" README.md | \
+                  sed -e 's/#/=/g' | \
+                  sed 's/^\(=*\)\(.*\)/\1\2 \1/' | \
+                  sed 's|.*https://packages.debian.org/sid/\(.*\))).*|\1|g' | \
+                  sed 's/^\([Aa-Zz]\)/ \* DebPkg:\1/')
+echo "$moinmoin_markup" | while read line; do 
+    if [[ "$line" =~ DebPkg ]]; then
+        package=${line#*:}
+        echo " * DebPkg:$package - $(_getdescription $package)"
+    else
+        echo "$line"
+    fi
+done
 
